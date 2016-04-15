17e65b4191af14855483b653d23d4e15bd7be80d
fangyao <FangyaoYan@users.noreply.github.com>
2016-04-15 21:51:07 +0800

    Create InvertBinaryTree.c

---
+++ InvertBinaryTree.c
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* invertTree(struct TreeNode* root) {
+    struct TreeNode* temp;
+    if(root == NULL)
+        return NULL;
+    temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+    root->left = invertTree(root->left);
+    root->right = invertTree(root->right);
+}
