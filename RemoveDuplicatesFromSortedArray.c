aca8dcd85f9802044eb3d9bf9bf974589aa309ca
fangyao <FangyaoYan@users.noreply.github.com>
2016-04-15 22:04:37 +0800

    Create RemoveDuplicatesFromSortedArray.c

---
+++ RemoveDuplicatesFromSortedArray.c
@@ -0,0 +1,15 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int i = 0, j = 1, realSize = numsSize;
+    
+    while(j < numsSize)
+    {
+        if (nums[j] == nums[i])
+        {
+            ++j;
+            --realSize;
+        }
+        else
+            nums[++i] = nums[j++];
+    }
+    return realSize;
+}
