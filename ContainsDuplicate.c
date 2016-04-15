e29f57037c6dbd09ef0569bd436c3dcb3ec4d105
fangyao <FangyaoYan@users.noreply.github.com>
2016-04-15 22:12:27 +0800

    Create ContainsDuplicate.c

---
+++ ContainsDuplicate.c
@@ -0,0 +1,11 @@
+bool containsDuplicate(int* nums, int numsSize) {
+  for (int i = 0; i < numsSize; ++i)
+	{
+		for (int j = i + 1; j < numsSize; ++j)
+		{
+			if (nums[i] == nums[j])
+				return 1;
+		}
+	}
+	return 0;
+}
